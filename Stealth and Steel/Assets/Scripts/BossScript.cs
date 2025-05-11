59fa0120d809a8c599d88d4ec50434803de67815
JozuaStandaert <jozua.standaert@student.howest.be>
2025-05-11 21:59:14 +0200

    Part 6
    Added Boss , still needs work

---
+++ Stealth and Steel/Assets/Scripts/BossScript.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class BossScript : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnDestroy()
+    {
+        Application.Quit();
+    }
+}
