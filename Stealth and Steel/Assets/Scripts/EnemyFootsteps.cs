736761fba6f638d5355e2b2b3bd878116597af53
Eduard Plams <plamseduard@gmail.com>
2025-06-05 23:05:34 +0200

    Steatlh Kill sound

---
+++ Stealth and Steel/Assets/Scripts/EnemyFootsteps.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyFootsteps : MonoBehaviour
+{
+    private NavMeshAgent _agent;       
+    public AudioSource FootstepsSFX;      
+    public float speedThreshold = 0.1f;
+
+    void Start()
+    {
+        _agent = GetComponent<NavMeshAgent>(); 
+    }
+
+    void Update()
+    {     
+        if (_agent.velocity.magnitude > speedThreshold)
+        {
+            
+            if (!FootstepsSFX.isPlaying)
+                FootstepsSFX.Play();
+        }
+        else
+        {
+            if (FootstepsSFX.isPlaying)
+                FootstepsSFX.Stop();
+        }
+    }
+}
